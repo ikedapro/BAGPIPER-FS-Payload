1b6ae8ad1294b39d9de8ca700f03ea8437ce7c42
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-03-15 18:13:22 -0400

    added file for servo class and some starting main code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # main script that runs upon pi turn on
 import time
-from imu import IMU
+# from imu import IMU
+from servo0 import servo0
 
 def main():
     # initialize imu
     imu = IMU()
-    DC,servo0 = imu.GetAdjustments()
+    DC,servo_0 = imu.GetAdjustments()
     
     # first set DC, then servos
     # TODO add detection for sudden large changes to imu, which indicates payload has moved, reset and readjust after that
     
     
-    print(DC, servo0)
+    print(DC, servo_0)
+    # initialize servo 0
+    s0 = servo0()
+    print(s0.debug())
 
     
 
