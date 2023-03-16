27ef0bc92043cd79f62a2b5beeeca7d90df1c7d1
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-03-16 04:56:06 -0400

    set up servo 0 class but no implementation, pushing to main for now (#1)
    * added file for servo class and some starting main code
    
    * cleaned code and left TODO comments for later work

---
+++ servo0.py
@@ -0,0 +1,32 @@
+from gpiozero import Servo
+import time
+# from gpiozero.pins.pigpio import PiGPIOFactory
+#factory = PiGPIOFactory()
+
+import RPi.GPIO as GPIO
+
+class Servo0:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(8, GPIO.OUT)
+        GPIO.output(8, 1)
+        time.sleep(5)
+        GPIO.output(8, 0)
+        GPIO.cleanup()
+    
+    def debug(self):
+        return "s0 class exists"
+    
+    def rotate(self, degrees):
+        
+        '''
+        #servo=Servo(23, pin_factory=factory)
+        servo=Servo(23)
+        servo.mid()
+        time.sleep(5)
+        servo.min()
+        time.sleep(3)
+        servo.max()
+        time.sleep(3)
+
+        '''
