af9d06c588f745a1eeec3ed429de913b08d62959
CMRocket <carnegiemellonrocket@gmail.com>
2023-03-17 00:16:07 -0400

    camera.py now takes images

--- camera.py
+++ camera.py
@@ -2,15 +2,4 @@ from picamera import PiCamera
 from time import sleep
 import os
 
-# camera = PiCamera()
-# camera.start_preview()
-# sleep(5)
-# camera.capture('/images/picture.jpg')
-# camera.stop_preview()
-
 os.system("libcamera-still -o images/test1.jpg --immediate")
-# use this command to test if camera is installed
-# $ vcgencmd get_camera
-
-# use this command to test camera
-# $ raspistill -o Desktop/image.jpg
