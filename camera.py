ac2d0fb434783ea2e6b8b0eec4368cf983c91c02
CMRocket <carnegiemellonrocket@gmail.com>
2023-03-16 23:51:03 -0400

    made camera.py take image

--- camera.py
+++ camera.py
@@ -1,12 +1,14 @@
 from picamera import PiCamera
 from time import sleep
+import os
 
-camera = PiCamera()
-camera.start_preview()
-sleep(5)
-camera.capture('/images/picture.jpg')
-camera.stop_preview()
+# camera = PiCamera()
+# camera.start_preview()
+# sleep(5)
+# camera.capture('/images/picture.jpg')
+# camera.stop_preview()
 
+os.system("libcamera-still -o images/test1.jpg --immediate")
 # use this command to test if camera is installed
 # $ vcgencmd get_camera
 
